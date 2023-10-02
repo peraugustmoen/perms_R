91ba7b40ea6b2ac009b7061fb86edd95723ab32d
Per August Moen <peraugust.pa@gmail.com>
2023-10-02 21:31:07 +0200

    docu

--- R/log_sum_exp.R
+++ R/log_sum_exp.R
@@ -4,9 +4,9 @@
 #' @description Computes the log sum exp of a vector. 
 #' Given input array = [x_1, ..., x_n], returns x_* + log(exp(x_1 - x_*) + ... + exp(x_n - x_*)),
 #' where x_* = max(x_1, ... x_n). Ignores entries
-#' with value -1, as these correspond to vanishing permanents.
+#' with NA value.
 #' @param x A vector length n containing the computed log permanents, where a zero permanent
-#'  is indicated by a -1.
+#'  is indicated by a NA value.
 #' @param x Input vector. 
 #' @return The log-sum-exp of the entries of the input vector. 
 #' @examples
